54e8a8f028e0b91895d266ea84ef36edaaf648d6
uehr <uehara.yudai@gmail.com>
2018-07-31 04:17:30 +0900

    update

--- example.nim
+++ example.nim
@@ -7,7 +7,9 @@ if existsEnv("PORT"):
   settings.port = Port(parseInt(getEnv("PORT")))
 
 routes:
-  get "/hoge":
-    resp h1("yay :)", a(href="http://github.com/vic/heroku-buildpack-nim", "hoge link"))
+  get "/":
+    let hoge = h1("yay :)", a(href="http://github.com/vic/heroku-buildpack-nim", "hoge link"))
+    echo hoge
+    resp hoge
 
 runForever()
