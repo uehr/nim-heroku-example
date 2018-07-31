cfb08d6f5c9eab69399e5b53ba884bb4a349579f
uehr <uehara.yudai@gmail.com>
2018-07-31 12:50:34 +0900

    hoge

--- example.nim
+++ example.nim
@@ -6,8 +6,9 @@ var settings = newSettings()
 if existsEnv("PORT"):
   settings.port = Port(parseInt(getEnv("PORT")))
 
+
 routes:
   get "/":
-    resp h1("yay :)")
+    resp h1("Hello jester :)")
 
 runForever()
